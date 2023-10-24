ffb89991420c9cdc15985b29742172f0388a7be4
inifarhan <farhangunawan619@gmail.com>
2023-10-24 14:20:51 +0700

    update: dont automatically archive product

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -50,18 +50,18 @@ export async function POST(req: Request) {
       }
     })
 
-    const productIds = order.orderItems.map((orderItem) => orderItem.productId)
+    // const productIds = order.orderItems.map((orderItem) => orderItem.productId)
 
-    await prismadb.product.updateMany({
-      where: {
-        id: {
-          in: [...productIds]
-        }
-      },
-      data: {
-        isArchived: true
-      }
-    })
+    // await prismadb.product.updateMany({
+    //   where: {
+    //     id: {
+    //       in: [...productIds]
+    //     }
+    //   },
+    //   data: {
+    //     isArchived: true
+    //   }
+    // })
   }
 
   return new NextResponse(null, { status: 200 })
